6d0d0aedfe16d2d85249cd3429777178c0ca4167
Jerry Kusenberger <gpdrew@gmail.com>
2023-07-13 20:54:01 -0500

    add pages and layout

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="wrapper">
+        <slot />
+    </div>
+  </template>
+  
+  
+  <style scoped>
+  .wrapper {
+    -webkit-overflow-scrolling: touch;
+    height: 100%;
+    overflow: auto;
+    width: 100%;
+  }
+  </style>
+  
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="flex items-center justify-center h-screen text-3xl pb-40">
+      Doghouse EQ2 Emu
+    </div>
+</template>
+  
