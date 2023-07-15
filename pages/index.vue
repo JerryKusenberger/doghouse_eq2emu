ca6af41e8402f17f380b9fd840413909057436bc
Jerry Kusenberger <gpdrew@gmail.com>
2023-07-15 18:16:18 -0500

    fetch list of characters and display on the page

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,26 @@
 <template>
   <div class="flex items-center justify-center h-screen pb-40">
     <div>
-      <div class="text-3xl">
+      <div class="text-3xl flex justify-center">
         Doghouse EQ2 Emu
       </div>
       <div class="flex items-center justify-center pt-5">
         <div>
-          <div class="pb-5">
+          <div class="pb-5 flex justify-center">
             <button class="outline outline-1 bg-gray-300 rounded p-1" @click="testApiConnection">Test API
               Connection</button>
           </div>
-          <div>
+          <div class="pb-5">
             {{ apiStatus }}
           </div>
+          <div class="pb-5 flex justify-center">
+            <button class="outline outline-1 bg-gray-300 rounded p-1" @click="getCharacters">Get Characters</button>
+          </div>
+          <div v-if="isLoading">Getting Characters...</div>
         </div>
+      </div>
+      <div v-for="character in characters" :key="character.id">
+        {{ character }}
       </div>
     </div>
   </div>
@@ -21,7 +28,11 @@
 
 
 <script setup>
+const { $api } = useNuxtApp()
+
 let apiStatus = ref('')
+let characters = ref([])
+let isLoading = ref(false)
 
 async function testApiConnection() {
   try {
@@ -30,6 +41,20 @@ async function testApiConnection() {
   }
   catch {
     apiStatus.value = 'API is down!'
+  }
+}
+
+async function getCharacters() {
+  try {
+    isLoading.value = true
+    characters.value = []
+    const response = await $api.characterApi.getCharacters()
+    characters.value = response
+  }
+  catch {
+  }
+  finally {
+    isLoading.value = false
   }
 }
 
